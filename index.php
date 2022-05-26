1a821827d631b6b2d2d846940e5a8c9e05eaa0ec
Mellya Vargas <103202202+MellyaV@users.noreply.github.com>
2022-05-26 18:30:10 +0000

    add new comment tomorrow

--- index.php
+++ index.php
@@ -4,10 +4,20 @@
   </head>
   <body>
     <h1>Form Handler</h1>
-    <form>
-      <p>First Name: <input type="text" name="FirstName"
-      /></p>
-      <p><input type="submit" /></p>
-    </form>
+
+  <?php
+    if(isset($_POST["FirstName"])){//Show data
+      echo $_POST["FirstName"];
+    }else{//Show form
+      echo '
+        <form method="post" action="">
+        <p>First Name: <input type="text" name="FirstName"
+        /></p>
+        <p><input type="submit" /></p>
+        </form>
+        ';
+    }
+  ?>
+    
   </body>
 </html>
